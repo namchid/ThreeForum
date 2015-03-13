1540cce83a5ea95fdf74dca3dca8644cb4a7b0a5
namchid <ndo@trinity.edu>
2015-03-13 15:24:21 -0500

    reverted to Brittney's newUserFormFields.php

--- Forum/resources/php/newUserFormFields.php
+++ Forum/resources/php/newUserFormFields.php
@@ -1,12 +1,12 @@
 <?php
-	echo '<form action="forum.php" method="post">';
-	echo '<input type="text" name="email" placeholder="email" class="input-fields">';
+	echo '<form name="newUserForm" action="" method="post">';
+	echo '<input type="text" id="email" name="email" placeholder="email" class="input-fields">';
         echo '<br>';
-        echo '<input type="text" name="username" placeholder="username" class="input-fields">';
+        echo '<input type="text" id="username" name="username" placeholder="username" class="input-fields">';
         echo '<br>';
-        echo '<input type="password" name="password1" placeholder="password" class="input-fields">';
+        echo '<input type="password" id="password1" name="password1" placeholder="password" class="input-fields">';
         echo '<br>';
-        echo '<input type="password" name="password2" placeholder="re-enter password" class="input-fields">';
-        echo '<button type="submit" id="submit-button">join the dark side.</button>';
+        echo '<input type="password" id="password2" name="password2" placeholder="re-enter password" class="input-fields">';
+        echo '<button id="submit-button" onclick="addUser()">join the dark side.</button>';
 	echo '</form>';
 ?>
