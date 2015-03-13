3b60152af0613dddc25c7b7ffbf0182dba67712a
HaydenHuff2 <hhuffine@Trinity.edu>
2015-03-13 13:52:52 -0500

    committing the old version of posts & profile

--- Forum/posts.php
+++ Forum/posts.php
@@ -1,6 +1,21 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
+<?php
+/** * User: Hayden H */
+?>
+
+<?php
+include_once("functions.php");
+include_once("connect.php");
+$topic_id ="";
+$topic_id = $_POST['topic_id'];
+$postsperpage = 10;
+$page = "1";
+$page = $_POST['page'];
+(int)$numpages = 1;
+$startinglimit = ((int)$page -1) * (int)$postsperpage ;
+?>
+    <!DOCTYPE HTML>
+    <html lang="en">
+    <head>
         <meta charset="utf-8">
         <title>Forum</title>
         <link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
@@ -8,69 +23,138 @@
         <link href='http://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="resources/styles/forum-styles.css">
         <link href="resources/styles/styles.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="resources/styles/new-topic-form-styles.css">
-	<link rel="stylesheet" type="text/css" href="resources/styles/post-styles.css">
+        <link rel="stylesheet" type="text/css" href="resources/styles/new-topic-form-styles.css">
+        <link rel="stylesheet" type="text/css" href="resources/styles/post-styles.css">
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
         <script type="text/javascript" src="resources/scripts/setUpNavigation-forum.js"></script>
         <script type="text/javascript" src="resources/scripts/posts.js"></script>
-	<script type="text/javascript" src="resources/scripts/inner-matrix.js"></script>
-</head>
-<body>
-	<div id="navBar"></div>
-	<div id="mainContainer">
-		<div class="subtitle">The Horrors of Debugging PHP</div>
-		<ul class="breadcrumb">
-			<li><a href="forum.php">Forum</a></li>
-			<li><a href="forum-2.php">Category</a></li>
-			<li><a>Posts</a></li> 
-		</ul>
-		<div id="pageNav">
-			<ul>
-				<li>page 1 of 1</li>
-				<li><a class="current" href="">1</a></li>
-			</ul>
-		</div>
-		<div id="posts">
-			<table class="posts-table">
-				<tr>
-					<td class="message-body">
-						<div class="row1">I'm the avatar!</div>
-					</td>		
-					<td class="post-num"><div class="row1">#1</div></td>
-				</tr>
-				<tr>
-					<td colspan="2" class="edit-post">
-                                                <a href="profile.php" class="username-field">aang</a>
-                                                <span class="edit" onclick="updatePostLabel()">edit post</span>
-                                        </td>
+        <script type="text/javascript" src="resources/scripts/inner-matrix.js"></script>
+    </head>
+    <body>
+    <div id="navBar"></div>
+    <div id="mainContainer">
+        <?php
+        $topic_name = "n/a";
+        $query_topic_name = 'SELECT * FROM topics WHERE topic_id = '.$topic_id.' ;';
+        $result_topic_name = mysqli_query($connect, $query_topic_name);
+        while($row_topic_name = mysqli_fetch_array($result_topic_name)) {
+            $topic_name = $row_topic_name['topic_subject'];
+        }
+        $result_topic_name->close();
+        $board_id = $board_name = $cat_name = $cat_id = "X";
+        $query_cat_board = ' SELECT c.*, b.* FROM topics t
+                        INNER JOIN categories c ON t.cat_id = c.cat_id
+                        INNER JOIN boards b ON c.board_id = b.board_id
+                        WHERE t.topic_id = '.$topic_id.';';
+        $result_cat_board = mysqli_query($connect, $query_cat_board);
+        while($row_cb = mysqli_fetch_array($result_cat_board)){
+            $board_id = $row_cb['board_id'];
+            $board_name = $row_cb['board_name'];
+            $cat_name = $row_cb['cat_name'];
+            $cat_id = $row_cb['cat_id'];
+        }
+        ?>
 
-				</tr>
-			</table>
-			<table class="posts-table">
-			 	<tr>
-                               	 	<td class="message-body">
-						<div class="row1">
-                                        	Wrong topic, Avatar. I have redeemed my honor!
-						</div>
-                                	</td>           
-                                	<td class="post-num"><div class="row1">#2</div></td>
-                        	</tr>
-                        	<tr> 
-                                	<td colspan="2" class="edit-post">
-                                		<a href="profile.php" class="username-field">zuko</a>
-						<span class="edit" onclick="updatePostLabel()">edit post</span>
+        <div class="subtitle"><?php echo $topic_name ?></div>
+        <ul class="breadcrumb">
+            <li><a href="forum.php">Forum</a></li>
+            <li><a href="#" class="toCategory">Category</a></li>
+            <li><a>Posts</a></li>
+        </ul>
+        <?php
+        $countquery =' SELECT count(*) AS cnt FROM posts WHERE topic_id = "'.$topic_id.'" ORDER BY post_id ASC ;';
+        $countresult = mysqli_query($connect, $countquery);
+        $realcount = "0";
+        while($countrow = mysqli_fetch_array($countresult)) {
+            $realcount = $countrow['cnt'];
+        }
+        (int)$numpages = (int)$realcount / (int)$postsperpage;
+        if($numpages > intval($numpages))
+            $numpages= intval($numpages +1);
+        $countresult->close();
+        $pagenames = array("page","topic_id");
+        $pagevals = array("1",     $topic_id);
+        echo'<div id="pageNav">
+			    <ul>
+			    	<li>page '.$page.' of '.$numpages.'</li> ';
+        for ($i =1; $i <= $numpages; $i++){
+            echo'<li class="page" value="'.$i.'" title ="'.$i.'"><a class="current" href="#">'.($i).'</a></li>';
+        }
+        echo'	</ul>
+		    </div>
+        ';
+        EchoForm("posts.php","pagesForm",$pagenames,$pagevals);
+        EchoForm("profile.php","profileForm", array("user_id"), array("x"));
+        EchoForm("category.php","categoryForm", array("cat_id","cat_name","board_id","board_name","myPage"), array($cat_id, $cat_name, $board_id, $board_name,"1"));
+        ?>
+
+        <div id="posts">
+            <table id="posts-table">
+                <?php
+                $postquery = ' SELECT * FROM posts WHERE topic_id = "'.$topic_id.'" ORDER BY post_id ASC  LIMIT '.$startinglimit.', '.$postsperpage.' ;';
+                $postresult = mysqli_query($connect,$postquery);
+                $postcount = 1;
+                while($row = mysqli_fetch_array($postresult)) {
+                    $userquery = ' SELECT * FROM users WHERE user_id = "'.$row['user_id'].'" ;';
+                    $userresult = mysqli_query($connect,$userquery);
+                    $username =  "";
+                    while($user = mysqli_fetch_array($userresult)) {
+                        if($user['user_name'] != "")
+                            $username = $user['user_name'];
+                    }
+                    $userresult->close();
+                    echo '
+                    <tr>
+					<td class="user">
+						<img src="resources/images/aang.png" class="user-image">
 					</td>
-                        	</tr>
-			</table>
-		</div>
-		<h3 id="new-post-label">New Post</h3>
-		<div id="post-form-area">
-                	<form action="" method="post" id="post-form">
-                        	<textarea rows="3" type="text" name="new-post" id="new-topic-post" form="post-form" placeholder="say something"></textarea>
-                		<button type="submit" name="submit-topic" class="submit-post-button">No Takebacks, OK?</button>
-			</form>
-		</div>
+					<td class="message-body">
+						'.$row['post_content'].'
+					</td>
+					<td class="post-num">#'.((int)$postcount * (int)$page).'</td>
+		    		</tr>
+			    	<tr>
+					<td colspan="3" class="edit-post">
+                       <span class="username-field" title="'.$row['user_id'].'">'.$username.'</span>
+                       <span class="edit" onclick="updatePostLabel()">edit post</span>
+                    </td>
+			    	</tr>
+                    ';
+                    $postcount +=1;
+                }
+                $postresult->close();
+                ?>
+
+            </table>
         </div>
-	<canvas id="c"></canvas>
-</body>
-</html>
+        <h3 id="new-post-label">New Post</h3>
+        <div id="post-form-area">
+            <form action="" method="post" id="post-form">
+                <textarea rows="3" type="text" name="new-post" id="new-topic-post" form="post-form" placeholder="say something"></textarea>
+                <button type="submit" name="submit-topic" class="submit-post-button">No Takebacks, OK?</button>
+            </form>
+        </div>
+    </div>
+    <canvas id="c"></canvas>
+    </body>
+    </html>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#pageNav li.page').click(function (event){
+                document.getElementById("pageNav").value = this.value;
+                if(this.value <= 0)
+                    $("#pageVal").val( this.title);
+                document.getElementById('pagesForm').submit();
+            });
+            $('*.username-field').click(function (event){
+                var userid = event.target.title;
+                $('#user_id').val(userid);
+                document.getElementById('profileForm').submit();
+            });
+            $('*.toCategory').click(function (event){
+                document.getElementById('categoryForm').submit();
+            });
+        });
+    </script>
+
+<?php
--- Forum/profile.php
+++ Forum/profile.php
@@ -1,69 +1,132 @@
+<?php
+include_once("functions.php");
+include_once("connect.php");
+$user_id = "";
+$user_id = $_POST["user_id"];
+$recent_limit = 10;
+?>
+<?php
+$user_email = $user_name = $user_date = "-1";
+$query_user = 'SELECT * FROM users where user_id ="'.$user_id.'";';
+$result_user = mysqli_query($connect, $query_user);
+while($row_user = mysqli_fetch_array($result_user)){
+    $user_email = $row_user['user_email'];
+    $user_date = $row_user['user_email'];
+    $user_name = $row_user['user_name'];
+}
+$result_user->close();
+$num_posts = -1;
+$query_post_count = 'SELECT count(*) AS cnt FROM posts WHERE user_id = "'.$user_id.'";' ;
+$result_post_count = mysqli_query($connect, $query_post_count);
+while($row_post_count = mysqli_fetch_array($result_post_count)){
+    $num_posts = $row_post_count['cnt'];
+}
+$result_post_count->close();
+$last_post_date = $last_topic = "-1";
+$query_last_post = 'SELECT * FROM posts WHERE user_id ="'.$user_id.'" ORDER BY post_id DESC LIMIT 1 ;' ;
+$result_last_post = mysqli_query($connect, $query_last_post);
+while($row_last_post = mysqli_fetch_array($result_last_post)){
+    $last_post_date = $row_last_post['post_date'];
+    $last_topic = $row_last_post['topic_id'];
+}
+$result_last_post->close();
+$last_temp = explode("-", $last_post_date);
+$last_year = (int)$last_temp[0];
+$last_month = (int)$last_temp[1];
+$last_day = (int)explode(" ", $last_temp[2])[0];
+$current_year = (int)date("Y");
+$current_day = (int)date("d");
+$current_month = (int)date("m");
+$last_post = "";
+if($current_day - $last_day)
+    $last_post = (int)($current_day - $last_day) . ' day(s) ' .$last_post;
+if($current_month - $last_month)
+    $last_post = $last_post .' '. (int)($current_month - $last_month) .' month(s)';
+if($current_year - $last_year)
+    $last_post = $last_post .' '. (int)($current_year - $last_year) .' year(s)' ;
+if($current_day-$last_day || $current_month-$last_month || $current_year-$last_year) {
+    $last_post = $last_post . " ago";
+    $last_post = $last_month. ' - '.$last_day.' - '.$last_year ;
+}
+else
+    $last_post = "today";
+?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-        <meta charset="utf-8">
-        <title>Forum</title>
-        <link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Cutive+Mono' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="resources/styles/forum-styles.css">
-	<link rel="stylesheet" type="text/css" href="resources/styles/profile-styles.css">
-        <link href="resources/styles/styles.css" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-        <script type="text/javascript" src="resources/scripts/setUpNavigation-profile.js"></script>
-        <script type="text/javascript" src="resources/scripts/inner-matrix.js"></script>
-	<script type="text/javascript" src="resources/scripts/profile.js"></script>
+    <meta charset="utf-8">
+    <title>Forum</title>
+    <link href='http://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Cutive+Mono' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="resources/styles/forum-styles.css">
+    <link rel="stylesheet" type="text/css" href="resources/styles/profile-styles.css">
+    <link href="resources/styles/styles.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+    <script type="text/javascript" src="resources/scripts/setUpNavigation-profile.js"></script>
+    <script type="text/javascript" src="resources/scripts/inner-matrix.js"></script>
+    <script type="text/javascript" src="resources/scripts/profile.js"></script>
 </head>
 <body>
-	<div id="navBar"></div>
-	<div id="mainContainer">
-		<div class="subtitle">hermajesty</div>
-		<div id="profile-area">
-			<div id="left-panel">
-			<!--	<div>
-					<img src="resources/images/aang.png">
-				</div> -->
-				<div id="stats-area">
-					<table id="user-stats-table">
-						<tr>
-							<td class="col-one">Joined:</td>
-							<td>[[9/8/1900]]</td>
-						</tr>
-						<tr>
-							<td class="col-one">Messages:</td>
-							<td>[[10]]</td>
-						</tr>
-						<tr>
-							<td class="col-one">Last Activity:</td>
-							<td>[[43 days ago]]</td>
-						</tr>
-					</table>		
-				</div>
-			</div>
-			<div id="right-panel">
-				<div id="about-user-area">
-					<h4>
-						About
-						<img class="edit-icon" src="resources/images/pencil54.png" title="edit" onclick="">
-					</h4>
-					Lorem ipsum
-				</div>
-				<div id="post-history">
-					<h4>History</h4>
-					<div class="history">
-						hermajesty posted in <a href="posts.php">The Horrors of Debugging PHP</a>
-					</div>
-					<div class="history">
-						hermajesty created new topic <a href="posts.php">The Horrors of Debugging PHP</a>
-					</div>
-					<div class="history">
-						hermajesty joined ThreeForum
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<canvas id="c"></canvas>
+<div id="navBar"></div>
+<div id="mainContainer">
+    <div class="subtitle"><?php echo $user_name; ?></div>
+    <div id="profile-area">
+        <div id="left-panel">
+            <div>
+                <img src="resources/images/aang.png">
+            </div>
+            <div id="stats-area">
+                <table id="user-stats-table">
+                    <tr>
+                        <td class="col-one">Email:</td>
+                        <td><?php echo $user_date; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="col-one">Total Posts:</td>
+                        <td><?php echo $num_posts; ?></td>
+                    </tr>
+                    <tr>
+                        <td class="col-one">Last Post:</td>
+                        <td><?php echo $last_post ; ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div id="right-panel">
+            <div id="about-user-area">
+                <h4>
+                    About
+                    <img class="edit-icon" src="resources/images/pencil54.png" title="edit" onclick="">
+                </h4>
+                Lorem ipsum
+            </div>
+            <div id="post-history">
+                <h4>Recent Posts</h4>
+                <?php
+                EchoForm("posts.php", "jumpToTopic", array("topic_id","page"), array("x", "1"));
+                $query_recent_posts = 'SELECT * FROM topics t INNER JOIN posts p ON t.topic_id = p.topic_id  WHERE p.user_id ='.$user_id.' ORDER BY p.post_id DESC LIMIT '.$recent_limit.' ;      ';
+                $result_recent_posts = mysqli_query($connect, $query_recent_posts);
+                while($row_recent = mysqli_fetch_array($result_recent_posts)){
+                    echo '<div class ="history">
+                               '.$user_name.' posted in <a href="#" class = "jump_to_topic" title="'.$row_recent['topic_id'].'" > '.$row_recent['topic_subject'].'  </a> </div> ';
+                }
+                $result_recent_posts->close();
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<canvas id="c"></canvas>
 </body>
 </html>
 
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('a.jump_to_topic').click(function (event){
+            var topic_id = this.title;
+            $('#topic_id').val(topic_id);
+            document.getElementById('jumpToTopic').submit();
+        });
+    });
+</script>
