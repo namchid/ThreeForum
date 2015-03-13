edff23602b1d5f840a51f046b504b2e62ca93618
namchid <ndo@trinity.edu>
2015-03-13 14:37:26 -0500

    here.

--- Forum/resources/php/loginFormFields.php
+++ Forum/resources/php/loginFormFields.php
@@ -1,13 +1,14 @@
 <?php
-include_one("connection.php");
+include "connection.php";
 ?>
 
+
 <?php
-	echo '<form action="forum.php" method="post">';
-	echo '<input type="text" name="username" placeholder="username" class="input-fields">';
+	echo '<form name=loginform action="" method="post">';
+	echo '<input type="text" id="username" name="username" placeholder="username" class="input-fields">';
         echo '<br>';
-	echo '<input type="password" name="password" placeholder="password" class="input-fields">';
+	echo '<input type="password" id="password" name="password" placeholder="password" class="input-fields">';
         echo '<br>';
-        echo '<button type="submit" id="submit-button">login</button>';
+        echo '<button id="submit-button" onclick="checkLogin()">login</button>';
 	echo '</form>';
 ?>
