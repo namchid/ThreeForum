6583e6f76dd478d23bdfe6865b9453778d562cde
bpauls <bpauls@trinity.edu>
2015-03-13 16:20:27 -0500

    fixing loginCheck

--- Forum/resources/php/loginCheck.php
+++ Forum/resources/php/loginCheck.php
@@ -1,7 +1,7 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+<?php  
+session_start();
+
+ 
 include_once "connection.php";
 $username = $_GET["username"];
 $password = $_GET["password"];
@@ -9,19 +9,28 @@ $password = $_GET["password"];
 $loginQuery = 'SELECT * FROM users WHERE user_name ='.'"'.$username.'"'.'&& user_pass ='.'"'.$password.'"';
 
 
-//$test_query = 'SELECT * FROM users WHERE user_name = "hitmonlee" && user_pass = "kickingfiend"';
+$test_query = 'SELECT * FROM users WHERE user_name = "hitmonlee" && user_pass = "kickingfiend"';
 
 //echo $loginQuery;
 $ret = mysqli_query($conn, $loginQuery);
-while($ret_row = mysqli_fetch_array($ret)){
-    $user_id = $ret_row['user_id'];
+$retRows = mysqli_num_rows($ret);
+
+function validCred ($retRows,$ret) {
+	if($retRows==1){
+  			while($rowCheck = mysqli_fetch_assoc($ret)) {
+                        $array = array();
+       			$array[] = $rowCheck;
+       }
+   $user_id = $array[0]["user_id"];
+   return $user_id; 
+  }
+
 }
 
+ $_SESSION['user_id'] = validCred($retRows, $ret);
 
-if($user_id != "-1") {
-    $_SESSION['user_id'] = $user_id;
-    echo "good";
-}
+if($retRows == 1)
+  echo "good";
 else 
   echo "bad";
 ?>
